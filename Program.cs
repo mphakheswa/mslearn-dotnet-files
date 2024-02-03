351ee412234d9623c3e373b52f018402cb35794b
Siphamandla Kheswa <spdk455@gmail.com>
2024-02-03 17:27:49 +0200

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+ using System.Linq;
+using System.Net;
+
+Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+// returns:
+// stores\201 on Windows
+//
+// stores/201 on macOS
+
+var currentDirectory = Directory.GetCurrentDirectory();
+
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesFiles = FindFiles(storesDirectory);
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }  
+
+    return salesFiles;
+}
